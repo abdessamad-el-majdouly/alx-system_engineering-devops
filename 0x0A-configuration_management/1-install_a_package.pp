aacfe84540c7f96a65512311c5ed0c51ca0ddbb5
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2023-11-25 01:14:33 +0100

    Update 1-install_a_package.pp3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-package {
-  'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Exec['apt-get update'],
-}
-
-exec {
-  'apt-get update':
-    command => '/usr/bin/apt-get update',
-    onlyif  => '/usr/bin/apt-get update',
-    path    => ['/usr/bin', '/bin'],
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
