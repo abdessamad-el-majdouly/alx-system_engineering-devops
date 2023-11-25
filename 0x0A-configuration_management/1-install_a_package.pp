2a92112674379c4e32f90db26d2f1db35ca505d0
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2023-11-25 01:10:30 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# 1-install_a_package.pp
+
+# Define a package resource for Flask
+package { 'Flask':
+  ensure   => '2.1.0',  # Specify the desired version
+  provider => 'pip3',   # Use pip3 as the package provider
+  require  => Package['python3-pip'],  # Ensure pip3 is installed before Flask
+}
+
+# Ensure that python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
 }
