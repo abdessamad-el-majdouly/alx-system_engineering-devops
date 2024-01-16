f99eaa8482bc43fabbf3685d8883132229e91080
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2024-01-16 16:30:30 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,15 +2,32 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception for HTTP errors (4xx or 5xx)
+
+        if response.status_code == 404:
+            return 0
+
+        results = response.json().get("data")
+        return results.get("subscribers")
+
+    except requests.RequestException as e:
+        print(f"Error: {e}")
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(subscribers)
